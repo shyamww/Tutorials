82c85b15a8695ecb348e9379029c71dd594a8988
shyamww <35889690+shyamww@users.noreply.github.com>
2021-08-03 18:05:23 +0530

    Update parking_lot.java

--- Java/parking_lot.java
+++ Java/parking_lot.java
@@ -7,6 +7,9 @@ class Simple{
     static Integer size_of_parking_lot;
 
 
+
+
+
     static void fn_park_new_car(String car_number, String car_colour){
         //adding new car
         Integer slot_num=q.poll();
@@ -32,7 +35,10 @@ class Simple{
         int n=Integer.parseInt(s);  
         boolean b = q.contains(n);
 
-        if(n<size_of_parking_lot){
+        
+
+
+        if(n<=size_of_parking_lot && n>0){
             // System.out.print("@@checking slot "+n+ " is occupied or not?: " + !b);
             if(!b){
                 q.add(n);
@@ -42,11 +48,15 @@ class Simple{
                 System.out.println("Slot number "+n+" is free");
                 // System.out.println("The size of the queue is: " + q.size());
             }
+            else{
+                System.out.println("Slot number "+n+" is already vacant");
+            }
             
         }
         else{
             System.out.println("Slot number is greater than the soze of the parking lot");
         }
+
 
     }
     static void fn_registration_numbers_for_cars_with_colour(String s){
@@ -87,6 +97,7 @@ class Simple{
             System.out.println("Not Found");
         }
 
+
     }
     static void fn_slot_number_for_registration_number(String s){
         Integer ck=0;
@@ -103,6 +114,7 @@ class Simple{
 
     }
     static void fn_status(){
+        
         System.out.println("Slot No.");
         List<Integer> arr = new ArrayList<Integer>();
         List<String> arr2 = new ArrayList<String>();
@@ -120,6 +132,9 @@ class Simple{
         for (int i=0;i<arr2.size();i++) {
             System.out.println(reg_number_to_colour.get(arr2.get(i)));
         }
+        
+
+                    
 
     }
     
@@ -129,32 +144,45 @@ class Simple{
         String ck="";
 
         do{
+            // System.out.print("\nEnter a string: ");  
             String str= sc.nextLine();           
             String[] words = str.split("\\s+");
-            
+            // System.out.print("You have entered: "+str);
+            // System.out.print(" words"+ words.length);
+
             if(words.length==3){//new car is added
                 if(q.size()>0)fn_park_new_car(words[1],words[2]);
                 else{
-                    System.out.println("Sorry, parking lot is full");
+                    if(slot_to_reg_num.size()>0){
+                        System.out.println("Sorry, parking lot is full");
+                    }
+                    else{
+                        System.out.println("Sorry, parking lot is not created");
+                    }
+                    
                 }
             }
             else if(words.length==2){
                 String a=words[0];
                 String b=words[1];
                 if(a.equals("create_parking_lot")){
+                // if(a.equals("c")){
                     fn_create_parking_lot(words[1]);
                 }
-                else if(a.equals("leave")){ 
+                else if(a.equals("leave")){ //done
                     fn_leave(b);
                 }
-                else if(a.equals("registration_numbers_for_cars_with_colour")){ 
+                else if(a.equals("registration_numbers_for_cars_with_colour")){ //done
                     fn_registration_numbers_for_cars_with_colour(b);
                 }
-                else if(a.equals("slot_numbers_for_cars_with_colour")){ 
+                else if(a.equals("slot_numbers_for_cars_with_colour")){ //done
                     fn_slot_numbers_for_cars_with_colour(b);
                 }
-                else if(a.equals("slot_number_for_registration_number")){ 
+                else if(a.equals("slot_number_for_registration_number")){ //done
                     fn_slot_number_for_registration_number(b);
+                }
+                else{
+                    System.out.println("Command not found");
                 }
             }
             else if(words.length==1){
@@ -164,23 +192,25 @@ class Simple{
                 else if(words[0].equals("exit")){
                     System.exit(0);
                 }
-                else if(words[0].equals("see")){
-                    for (Integer i : slot_to_reg_num.keySet()) {
-                        System.out.println("slot: " + i + " number: " + slot_to_reg_num.get(i));
-                    }
-                    for (String i : reg_number_to_colour.keySet()) {
-                        System.out.println("number: " + i + " colour: " + reg_number_to_colour.get(i));
-                    }
-                    
+                else{
+                    System.out.println("Command not found");
                 }
             }
+            else{
+                System.out.println("Command not found");
+            }
+            
 
         }while(ck!="exit");
+
+
     
     }  
 }  
 
 
+
+ 
 
 
 // create_parking_lot 6
@@ -199,27 +229,3 @@ class Simple{
 // slot_number_for_registration_number KA-01-HH-3141
 // slot_number_for_registration_number MH-04-AY-1111
 // exit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
